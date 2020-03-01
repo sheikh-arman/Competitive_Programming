47fc2d15c29305ff8f13d52667e03a9914871e1e
Sk Arman Hossain <39202170+armanBU@users.noreply.github.com>
2020-03-01 22:50:26 +0600

    Add files via upload

---
+++ Datastructure_Algorithms_NumberTheory/DataStructure/DisjointSetUnion(DSU)/hackerearth_Friendless_Dr_Sheldon_Cooper_solution_disjoint_set_union_accepted_in_c++.cpp
@@ -0,0 +1,58 @@
+
+/*
+    Sk arman Hossain
+    University of Barisal
+
+    Problem name :  Friendless_Dr_Sheldon_Cooper
+    Algorithm : DSU
+ */
+
+#include<bits/stdc++.h>
+#define nl printf("\n");
+#define N 200001
+#define PR pair<int,int>
+#define sf(n) scanf("%d", &n)
+#define sff(n, m) scanf("%d %d",&n,&m)
+#define sfl(n) scanf("%lld", &n)
+#define sfu(n) scanf("%llu", &n)
+#define sffl(n, m) scanf("%lld %lld",&n,&m)
+#define pfn(n) printf("%d\n",n)
+#define pf(n) printf("%d ",n)
+#define pfl(n) printf("%lld ",n)
+#define pfln(n) printf("%lld\n",n)
+#define pfu(n) printf("%llu\n",n)
+#define pfs(s) printf("%s",s)
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define PB push_back
+#define PI 3.1415926536
+#define VST(v) sort(v.begin(),v.end())
+#define fori(i,n) for(;i<n;i++)
+#define fast ios_base::sync_with_stdio(0);cin.tie(0)
+#define MP make_pair
+#define ff first
+#define ss second
+int dx[]= {0,0,1,-1};
+int dy[]= {1,-1,0,0};
+typedef long long int ll;
+typedef unsigned long long int ull;
+using namespace std;
+
+int main()
+{
+    ///freopen("1input.txt","r",stdin);
+    int tcase=1;
+    sf(tcase);
+    for(int test=1;test<=tcase;test++)
+    {
+       int n,m;
+       sff(n,m);
+       for(int i=0;i<m;i++){
+        int a,b;
+        sff(a,b);
+       }
+        printf("%d\n",n-1);
+    }
+///*****************************  ALHAMDULILLAH  *****************************/
+}
+
