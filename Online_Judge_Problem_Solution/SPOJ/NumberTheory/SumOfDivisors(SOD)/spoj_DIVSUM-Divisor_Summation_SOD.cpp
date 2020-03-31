158a9df64b18d68b1ecb946932251e5bf04d64ef
Sk Arman Hossain <39202170+armanBU@users.noreply.github.com>
2020-04-01 00:56:07 +0600

    Add files via upload

---
+++ Online_Judge_Problem_Solution/SPOJ/NumberTheory/SumOfDivisors(SOD)/spoj_DIVSUM-Divisor_Summation_SOD.cpp
@@ -0,0 +1,72 @@
+
+/*
+Author: Sk Arman Hossain
+University of Barishal
+Problem : DIVSUM - Divisor Summation
+Algorithm: Sum of divisor(sod)
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define PB push_back
+ll N=500000;
+vector<ll>prime;
+bitset<500010>ckprime;
+void seive()
+{
+    prime.PB(2);
+    for(ll i=3; i<=N; i+=2)
+    {
+        if(ckprime[i]==false)
+        {
+            prime.PB(i);
+            for(ll j=i*i; j<=N; j+=i+i)
+            {
+                ckprime[j]=true;
+            }
+        }
+    }
+}
+ll sod(ll n)
+{
+    ll ans=1;
+    ll sq=sqrt(n+1);
+    ll siz=prime.size();
+    for(ll i=0; prime[i]<=sq; i++)
+    {
+        ll cnt=0;
+        if(n%prime[i]==0)
+        {
+            while(n%prime[i]==0){
+                cnt++;
+                n/=prime[i];
+            }
+            ans*=(powl(prime[i],cnt+1)-1)/(prime[i]-1);
+        }
+        if(i==siz)break;
+    }
+    if(n>1)
+    {
+        ans*=(powl(n,2)-1)/(n-1);
+    }
+    return ans;
+
+}
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    seive();
+    ll tcase;
+    cin>>tcase;
+    while(tcase--)
+    {
+        ll n,m;
+        cin>>n;
+        cout<<sod(n)-n<<"\n";
+    }
+    return 0;
+}
+
+
+
