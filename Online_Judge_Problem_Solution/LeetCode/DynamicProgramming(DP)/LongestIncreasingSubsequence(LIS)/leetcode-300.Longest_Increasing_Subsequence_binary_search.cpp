9a7c46ae38e5468482f52e709a5f43a6a4c03080
Sk Arman Hossain <39202170+armanBU@users.noreply.github.com>
2020-04-20 21:22:10 +0600

    Add files via upload

---
+++ Online_Judge_Problem_Solution/LeetCode/DynamicProgramming(DP)/LongestIncreasingSubsequence(LIS)/leetcode-300.Longest_Increasing_Subsequence_binary_search.cpp
@@ -0,0 +1,27 @@
+
+/*
+    Sk arman Hossain
+    University of Barisal
+
+    Problem : LeetCode->300. Longest Increasing Subsequence
+    Solution : Binary Search
+ */
+ class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>V;
+        int siz=nums.size();
+        for(int i=0;i<siz;i++){
+            int pos=lower_bound(V.begin(),V.end(),nums[i])-V.begin();
+            int s=V.size();
+            if(pos>=s){
+                V.push_back(nums[i]);
+            }
+            else{
+                V[pos]=nums[i];
+            }
+        }
+        int ans=V.size();
+        return ans;
+    }
+};
