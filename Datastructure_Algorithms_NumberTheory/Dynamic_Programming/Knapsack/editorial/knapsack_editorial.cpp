571e4682adf58fbb501b5bd52a8055226fa2976b
Sk Arman Hossain <39202170+armanBU@users.noreply.github.com>
2020-04-27 03:07:58 +0600

    Add files via upload

---
+++ Datastructure_Algorithms_NumberTheory/Dynamic_Programming/Knapsack/editorial/knapsack_editorial.cpp
@@ -0,0 +1,54 @@
+/*
+    Sk arman Hossain
+    University of Barisal
+
+    Problem name : knapsak
+    Algorithm : dp
+ */
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define N 2002
+ll dp[N+10][N+10];
+ll siz[N+10];
+ll weight[N+10];
+ll mx_weight,n;
+ll knap(ll i,ll w){
+    if(i>=n){
+        return 0;
+    }
+    if(dp[i][w]!=-1){
+        return dp[i][w];
+    }
+    ll ans1=0,ans2=0;
+    ans1=knap(i+1,w);
+    if(w>=siz[i]){
+        ans2=knap(i+1,w-siz[i])+weight[i];
+    }
+    return dp[i][w]=max(ans1,ans2);
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll tcase=1;
+    //cin>>tcase;
+    for(ll TEST=1; TEST<=tcase; TEST++)
+    {
+        cin>>n>>mx_weight;
+        for(ll i=0;i<=n;i++){
+            for(ll j=0;j<=mx_weight;j++){
+                dp[i][j]=-1;
+            }
+        }
+        for(ll i=0;i<n;i++){
+            cin>>siz[i]>>weight[i];
+        }
+        ll ans=knap(0,mx_weight);
+        cout<<ans<<endl;
+    }
+///*****************************  ALHAMDULILLAH  *****************************/
+    return 0;
+}
+
+
+
