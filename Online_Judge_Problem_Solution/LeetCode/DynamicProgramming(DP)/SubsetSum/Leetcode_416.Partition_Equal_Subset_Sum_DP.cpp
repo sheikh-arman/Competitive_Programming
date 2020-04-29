535e6563fd563cf671c5f6d7f4e62a8706b4ae21
Sk Arman Hossain <39202170+armanBU@users.noreply.github.com>
2020-04-30 02:53:39 +0600

    Update Leetcode_416.Partition_Equal_Subset_Sum_DP.cpp

--- Online_Judge_Problem_Solution/LeetCode/DynamicProgramming(DP)/SubsetSum/Leetcode_416.Partition_Equal_Subset_Sum_DP.cpp
+++ Online_Judge_Problem_Solution/LeetCode/DynamicProgramming(DP)/SubsetSum/Leetcode_416.Partition_Equal_Subset_Sum_DP.cpp
@@ -10,48 +10,46 @@ class Solution
 {
 public:
     typedef long long int ll;
-    ll dp[210][40000],sum=0,n;
-    vector<ll>V;
-    bool sub_sum(ll i,ll val)
+ll dp[20010],sum,n;
+vector<ll>V;
+bool sub_sum(ll i,ll val)
+{
+    if(i>=n)
     {
-        if(i>=n)
+        //cout<<val<<" t\n";
+        if(val==sum/2)
         {
-            if(val==sum/2)
-            {
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }
+            return 1;
         }
-        if(dp[i][val]!=-1)
+        else
         {
-            return dp[i][val];
+            return 0;
         }
-        bool ans=0;
-        if(val+V[i]<=sum/2)
-        {
-            ans=sub_sum(i+1,val+V[i]);
-        }
-        ans|=sub_sum(i+1,val);
-        return dp[i][val]=ans;
     }
+    if(dp[val]!=-1)
+    {
+        return dp[val];
+    }
+    bool ans=0;
+    if(val+V[i]<=sum/2)
+    {
+        ans=sub_sum(i+1,val+V[i]);
+    }
+    ans|=sub_sum(i+1,val);
+    return dp[val]=ans;
+}
     bool canPartition(vector<int>& nums)
     {
         sum=0;
         n=nums.size();
-        for(ll i=0; i<=n; i++)
-        {
-            for(ll j=0; j<=(n*100)/2; j++)
-            {
-                dp[i][j]=-1;
-            }
-        }
         for(ll i=0; i<n; i++)
         {
             V.push_back(nums[i]);
             sum+=nums[i];
+        }
+        for(ll j=0; j<=sum/2; j++)
+        {
+            dp[j]=-1;
         }
         if(sum%2!=0)
         {
