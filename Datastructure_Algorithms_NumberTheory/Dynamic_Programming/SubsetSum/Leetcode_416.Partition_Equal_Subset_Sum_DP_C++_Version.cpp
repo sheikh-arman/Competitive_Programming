7f7e80e3ff80fc1df7e12ea310ad83ad1f97880d
Sk Arman Hossain <39202170+armanBU@users.noreply.github.com>
2020-04-30 02:52:27 +0600

    Update Leetcode_416.Partition_Equal_Subset_Sum_DP_C++_Version.cpp

--- Datastructure_Algorithms_NumberTheory/Dynamic_Programming/SubsetSum/Leetcode_416.Partition_Equal_Subset_Sum_DP_C++_Version.cpp
+++ Datastructure_Algorithms_NumberTheory/Dynamic_Programming/SubsetSum/Leetcode_416.Partition_Equal_Subset_Sum_DP_C++_Version.cpp
@@ -8,27 +8,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-ll dp[210][40000],sum,n;
+ll dp[20010],sum,n;
 vector<ll>V;
-bool sub_sum(ll i,ll val){
-    if(i>=n){
-            //cout<<val<<" t\n";
-        if(val==sum/2){
+bool sub_sum(ll i,ll val)
+{
+    if(i>=n)
+    {
+        //cout<<val<<" t\n";
+        if(val==sum/2)
+        {
             return 1;
         }
-        else{
+        else
+        {
             return 0;
         }
     }
-    if(dp[i][val]!=-1){
-        return dp[i][val];
+    if(dp[val]!=-1)
+    {
+        return dp[val];
     }
     bool ans=0;
-    if(val+V[i]<=sum/2){
+    if(val+V[i]<=sum/2)
+    {
         ans=sub_sum(i+1,val+V[i]);
     }
     ans|=sub_sum(i+1,val);
-    return dp[i][val]=ans;
+    return dp[val]=ans;
 }
 
 int main()
@@ -44,19 +50,16 @@ int main()
     {
         sum=0;
         cin>>n;
-        for(ll i=0; i<=n; i++)
-        {
-            for(ll j=0; j<=(n*100)/2; j++)
-            {
-                dp[i][j]=-1;
-            }
-        }
         for(ll i=0; i<n; i++)
         {
             ll a;
             cin>>a;
             V.push_back(a);
             sum+=a;
+        }
+        for(ll j=0; j<=sum/2; j++)
+        {
+            dp[j]=-1;
         }
         if(sum%2!=0)
         {
