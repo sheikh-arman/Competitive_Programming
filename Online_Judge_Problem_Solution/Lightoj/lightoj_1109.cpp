608a534f633ab4b0ed61affecb3f046f11c081e8
Sk Arman Hossain <39202170+armanBU@users.noreply.github.com>
2020-07-06 22:10:26 +0600

    Add files via upload

---
+++ Online_Judge_Problem_Solution/Lightoj/lightoj_1109.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#define nl printf("\n");
+#define N 200001
+#define mp pair<ll,ll>
+#define sf(n) scanf("%d", &n)
+#define sff(n, m) scanf("%d %d",&n,&m)
+#define sfl(n) scanf("%lld", &n)
+#define sfu(n) scanf("%llu", &n)
+#define sffl(n, m) scanf("%lld %lld",&n,&m)
+#define pfn(n) printf("%d\n",n)
+#define pf(n) printf("%d ",n)
+#define pfl(n) printf("%lld ",n)
+#define pfln(n) printf("%lld\n",n)
+#define pfu(n) printf("%llu\n",n)
+#define pfs(s) printf("%s",s)
+#define pb push_back
+#define pi 3.1415926536
+#define vst(v) sort(v.begin(),v.end())
+#define fori(n) for(int i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(0);cin.tie(0)
+int dx[]= {0,0,1,1};
+int dy[]= {1,1,0,0};
+typedef long long int ll;
+typedef unsigned long long int ull;
+using namespace std;
+const int N1=1000;
+vector<int>prime_list;
+vector<int>nod[40];
+int NOD[N1+5];
+void sieve(){
+    //prime_list.push_back(2);
+    for(int i=2;i<=N1;i+=1){
+            //NOD[i]++;
+            bool ck=true;
+        if(NOD[i]==0) {prime_list.push_back(i);ck=false;}
+        for(int j=i;j<=N1;j+=i){
+            NOD[j]++;
+        }
+        if(ck)
+        nod[NOD[i]].pb(i);
+    }
+    prime_list.pb(1);
+    reverse(prime_list.begin(),prime_list.end());
+    for(int i=2;i<40;i++){
+        int siz=nod[i].size();
+        for(int j=siz-1;j>=0;j--)
+            prime_list.pb(nod[i][j]);
+    }
+}
+int main()
+{
+    ///freopen("input.txt","r",stdin);
+    sieve();
+//    for(int i=0;i<=1000;i++)
+//        printf("%d %d %d\n",i,prime_list[i],NOD[prime_list[i]]);
+    int t,cnt=0;
+    sf(t);
+    while(t--)
+    {
+        int n;
+        sf(n);
+        printf("Case %d: %d\n",++cnt,prime_list[n-1]);
+    }
+///*****************************  ALHAMDULILLAH  *****************************/
+}
