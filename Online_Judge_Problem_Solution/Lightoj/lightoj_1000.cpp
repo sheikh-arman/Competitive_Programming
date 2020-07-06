30f8628f14abddabe9cf15803d5e952bbf02496e
Sk Arman Hossain <39202170+armanBU@users.noreply.github.com>
2020-07-06 22:01:59 +0600

    Add files via upload

---
+++ Online_Judge_Problem_Solution/Lightoj/lightoj_1000.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int t,a,b,i=1;
+    cin>>t;
+    while(t--){
+        scanf("%d%d",&a,&b);
+        printf("Case %d: %d\n",i++,a+b);
+    }
+
+    return 0;
+}
