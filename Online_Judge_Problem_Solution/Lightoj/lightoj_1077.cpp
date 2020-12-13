0613e0fe5c0de96c98bd38da5e8d63a0723b9e0e
sheikh-arman <arman.cse4.bu@gmail.com>
2020-12-13 17:34:22 +0600

    lightoj 1077 added

---
+++ Online_Judge_Problem_Solution/Lightoj/lightoj_1077.cpp
@@ -0,0 +1,62 @@
+/*
+    Sk arman Hossain
+    University of Barisal
+
+    Problem :
+    Solution :
+ */
+#include<bits/stdc++.h>
+#define nl cout<<"\n";
+#define N 200001
+#define PR pair<ll,ll>
+#define sf1(n) cin>>n
+#define sf2(n, m) cin>>n>>m
+#define sf3(n, m,k ) cin>>n>>m>>k
+#define pfn(n) printf("%d\n",n)s
+#define pf1(n) printf("%d ",n)
+#define pfl1(n) printf("%lld ",n)
+#define pfln(n) printf("%lld\n",n)
+#define pfu(n) printf("%llu\n",n)
+#define pfs(s) printf("%s",s)
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define minus1 printf("-1\n");
+#define PB push_back
+#define PI 3.1415926536
+#define VST(v) sort(v.begin(),v.end())
+#define VSTcmp(v,cmp) sort(v.begin(),v.end(),cmp)
+#define fori(i,n) for(;i<n;i++)
+#define fast ios_base::sync_with_stdio(0);cin.tie(0)
+#define MP make_pair
+#define ff first
+#define ss second
+#define tt third
+#define mod 1000000007
+#define T(n) printf("test %d\n",n)
+int dx[] = {0,0,1,-1,2,2,-2,-2,1,-1,1,-1};
+int dy[] = {1,-1,0,0,1,-1,1,-1,2,2,-2,-2};
+typedef long long int ll;
+typedef unsigned long long int ull;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);
+//    freopen("1input.txt","r",stdin);
+//    freopen("1output.txt","w",stdout);
+    ll tcase=100;
+    //sf1(tcase);
+    cin>>tcase;
+    for(ll test=1; test<=tcase; test++)
+    {
+        ll x1,y1,x2,y2;
+        cin>>x1>>y1>>x2>>y2;
+        ll dis1=abs(x1-x2);
+        ll dis2=abs(y1-y2);
+        ll ans=__gcd(min(dis1,dis2),max(dis1,dis2))-1;
+        cout<<"Case "<<test<<": "<<ans+2<<endl;
+    }
+///*****************************  ALHAMDULILLAH  *****************************/
+}
+
+
+
