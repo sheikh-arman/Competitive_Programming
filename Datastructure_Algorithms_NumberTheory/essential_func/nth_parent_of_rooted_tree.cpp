61cde860f3247c30c0f52d415c94fb5d5b1802b5
sheikh-arman <arman.cse4.bu@gmail.com>
2021-01-07 02:54:04 +0600

    nth parent of tree added

---
+++ Datastructure_Algorithms_NumberTheory/essential_func/nth_parent_of_rooted_tree.cpp
@@ -0,0 +1,148 @@
+/*
+    Sk arman Hossain
+    University of Barisal
+    Problem name :
+    Algorithm :
+ */
+#include<bits/stdc++.h>
+#define nl printf("\n");
+#define PR pair<ll,ll>
+#define sf1(n) scanf("%lld", &n)
+#define sf2(n, m) scanf("%lld %lld",&n,&m)
+#define sf3(n, m,k ) scanf("%lld %lld %lld",&n,&m,&k)
+#define pfn(n) printf("%d\n",n)s
+#define pf1(n) printf("%d ",n)
+#define pfl1(n) printf("%lld ",n)
+#define pfln(n) printf("%lld\n",n)
+#define pfu(n) printf("%llu\n",n)
+#define pfs(s) printf("%s",s)
+#define YES printf("YES\n")
+#define NO printf("NO\n")
+#define minus1 printf("-1\n")
+#define PB push_back
+#define PI 3.1415926536
+#define VST(v) sort(v.begin(),v.end())
+#define VSTcmp(v,cmp) sort(v.begin(),v.end(),cmp)
+#define fori(i,n) for(;i<n;i++)
+#define fast ios_base::sync_with_stdio(0);cin.tie(0)
+#define MP make_pair
+#define ff first
+#define ss second
+#define tt third
+#define mod 1000000007
+#define T(n) printf("test %d\n",n)
+typedef long long int ll;
+typedef unsigned long long int ull;
+ll dx[] = {0,0,1,-1,2,2,-2,-2,1,-1,1,-1};
+ll dy[] = {1,-1,0,0,1,-1,1,-1,2,2,-2,-2};
+using namespace std;
+#define N 200100
+vector<ll>edj[N];
+bool color[N];
+ll label[N];
+ll distance[N];
+ll sparse[N][20];
+ll parent[N];
+void dfs(ll node)
+{
+    color[node]=false;
+    ll siz=edj[node].size();
+    for(ll i=0; i<siz; i++)
+    {
+        ll tm=edj[node][i];
+        if(color[tm]==true)
+        {
+            color[tm]=false;
+            label[tm]=label[node]+1;
+            parent[tm]=node;
+            sparse[tm][0]=node;
+            dfs(tm);
+        }
+    }
+}
+void pre_process(ll n)
+{
+    for(ll j=1; (1<<j)<=n; j++)
+    {
+        for(ll i=1; i<=n; i++)
+        {
+            sparse[i][j]=sparse[sparse[i][j-1]][j-1];
+        }
+    }
+}
+ll nth_parent(ll u,ll nth)
+{
+    ll lg=0;
+    for(lg=0; (1<<lg)<=label[u]; lg++)
+    {
+
+    }
+    lg--;
+    ll nth_parent_label=label[u]-nth;
+    if(label[u]!=nth_parent_label)
+    {
+        for(ll i=lg; i>=0; i--)
+        {
+            if(label[u]-(1<<i)>=nth_parent_label)
+            {
+                u=sparse[u][i];
+            }
+        }
+    }
+    return u;
+}
+int main()
+{
+    // fast;
+    //freopen("1input.txt","r",stdin);
+    ///freopen("1output.txt","w",stdout);
+    ll tcase=1;
+    sf1(tcase);
+    //cin>>tcase;
+    for(ll test=1; test<=tcase; test++)
+    {
+        ll n,e;
+        sf1(n);
+        for(ll i=0;i<=n;i++){
+            edj[i].clear();
+            color[i]=true;
+            label[i]=-1;
+            memset(sparse[i],-1,sizeof(sparse[i]));
+        }
+        for(ll i=0; i<n-1; i++)
+        {
+            ll u,v;
+            sf2(u,v);
+            edj[u].PB(v);
+            edj[v].PB(u);
+        }
+        label[1]=0;
+        parent[1]=1;
+        dfs(1);
+        pre_process(n);
+        ll qq;
+        printf("Case %lld:\n",test);
+        sf1(qq);
+        for(ll i=0; i<qq; i++)
+        {
+            ll u,nth_parnt;
+            sf2(u,nth_parnt);
+            ll lc=nth_parent(u,nth_parnt);
+            printf("%lld\n",lc);
+        }
+    }
+///*****************************  ALHAMDULILLAH  *****************************/
+}
+/*
+1
+8
+1 2
+2 4
+2 6
+1 3
+3 5
+5 8
+5 7
+1
+1 2
+*/
