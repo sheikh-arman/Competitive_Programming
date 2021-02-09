a23a4a1d4df2c2b156444f19f411dafef5604fa6
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2021-02-09 17:38:14 +0600

    Add files via upload

---
+++ Datastructure_Algorithms_NumberTheory/essential_func/combination_binomial_coefficient.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+# define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+typedef long long int ll;
+ll mem[330][330];
+ll dp(ll n,ll k){
+    if(k>n){
+        return 0;
+    }
+    if(k==0||k==n){
+        return 1;
+    }
+    if(mem[n][k]>0){
+        return mem[n][k];
+    }
+    mem[n][k]=dp(n-1,k)+dp(n-1,k-1);
+    return mem[n][k];
+}
+int main(){
+    fast;
+    //freopen("1input.txt","r",stdin);
+//    freopen("1output.txt","w",stdout);
+    ll tcase=1;
+    cin>>tcase;
+    for(ll test=1;test<=tcase;test++){
+        ll n,k;
+        cin>>n>>k;
+        ll ans=0;
+        ans=dp(n,k);
+        cout<<"Case "<<test<<": "<<ans<<"\n";
+    }
+    return 0;
+}
+
