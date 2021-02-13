7bb8a92aa1bf7bf4ca02779eb9ef200804737b49
Sk Arman Hossain <arman.cse4.bu@gmail.com>
2021-02-13 19:00:30 +0600

    Add files via upload

---
+++ Datastructure_Algorithms_NumberTheory/essential_func/memory_calculation.cpp
@@ -0,0 +1,11 @@
+
+ /// memory Calculation
+    /*
+    cout<<prime.size()<<" "<<precal.size()<<"  \n";
+    int x=(sizeof(ck))/1000;
+    int y=sizeof(unsigned int)*prime.size();
+    y/=1000;
+    int z=sizeof(unsigned int)*precal.size();
+    z/=1000;
+    cout<<(x+y+z)<<endl;
+    */
