5fa62d083d20a9fccc400ebd1621f7f342af1be3
unknown <arman.cse4.bu@gmail.com>
2022-04-25 03:08:44 +0600

    hackerearth aprill 2022 2

---
+++ Practice/Hackerearth/cannibal-characters-d30e8a5f.cpp
@@ -0,0 +1,56 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define sf(n) scanf("%lld",&n);
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define nl cout<<"\n";
+#define PB push_back
+#define VST(V) sort(V.begin(),V.end())
+#define VSTrev(V) sort(V.begin(),V.end(),greater<long long int>())
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define  base1 129
+#define  base2 137
+#define  MOD1  1479386893
+#define  MOD2  1928476349
+#define MAX 2000010
+#define T(n) printf("test : %d\n",n);
+ll dx[]= {1,-1,0,0,1,-1,-1,1};
+ll dy[]= {0,0,1,-1,1,1,-1,-1};
+ll knx[]= {2,2,1,-1,-2,-2,1,-1};
+ll kny[]= {1,-1,2,2,1,-1,-2,-2};
+int main()
+{
+    //freopen("1input.txt","r",stdin);
+    //freopen("1output.txt","w",stdout);
+    fast;
+    ll tcase=1;
+    cin>>tcase;
+    for(ll test=1; test<=tcase; test++)
+    {
+        ll n;
+        cin>>n;
+        string s;
+        cin>>s;
+        ll ans=0;
+        ll ar[30];
+        for(ll i=0;i<26;i++){
+            ar[i]=0;
+        }
+        for(ll i=0;i<n;i++){
+            ar[s[i]-'a']++;
+        }
+        for(ll i=0;i<26;i++){
+            if(ar[i]){
+                ans+=ar[i]/2;
+            }
+        }
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
+
+
+
+
