6fefaa3588657cf06874fb043a2cda2e7c57f7d8
unknown <arman.cse4.bu@gmail.com>
2022-04-29 02:54:12 +0600

    hackerearth2

---
+++ Online_Judge_Problem_Solution/HackerEarth/string-challenge-fbf8cc02.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define sf(n) scanf("%lld",&n);
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define nl cout<<"\n";
+#define PB push_back
+#define VST(V) sort(V.begin(),V.end())
+#define VSTrev(V) sort(V.begin(),V.end(),greater<long long int>())
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define  base1 129
+#define  base2 137
+#define  MOD1  1479386893
+#define  MOD2  1928476349
+#define MAX 2000010
+#define T(n) printf("test : %d\n",n);
+ll dx[]= {1,-1,0,0,1,-1,-1,1};
+ll dy[]= {0,0,1,-1,1,1,-1,-1};
+ll knx[]= {2,2,1,-1,-2,-2,1,-1};
+ll kny[]= {1,-1,2,2,1,-1,-2,-2};
+int main()
+{
+    freopen("1input.txt","r",stdin);
+    //freopen("1output.txt","w",stdout);
+    //fast;
+    ll tcase=1;
+    cin>>tcase;
+
+    for(ll test=1; test<=tcase; test++)
+    {
+        ll n,k;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        ll len=1,ck=1;
+        for(ll i=1; i<n&&ck; i++)
+        {
+            if(s[i]>s[i-1])
+            {
+                if(len==1)break;
+                ll checking=0;
+                for(ll j=0; j<len&&i<n; j++)
+                {
+                    if(s[j]<s[i])
+                    {
+                        ck=0;
+                        break;
+                    }
+                    else
+                    {
+                        if(s[i]<s[j])
+                        {
+                            checking=1;i++;break;
+                        }
+                        i++;
+                    }
+                }
+                if(ck)
+                {
+                    if(checking==1)
+                    {
+                        len=i;
+                        i--;
+                    }
+                    else
+                    {
+                        ck=0;
+                        break;
+                    }
+
+                }
+            }
+            else
+            {
+                len=i+1;
+            }
+        }
+        //cout<<len<<" x\n";
+        ll cnt=0;
+        for(ll i=0; i<k+n; i++)
+        {
+            cout<<s[cnt];
+            cnt++;
+            cnt%=len;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+
+
+
