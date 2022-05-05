b36f00babba13958b16edd4a7d7308a6bc1ea87c
unknown <arman.cse4.bu@gmail.com>
2022-05-05 14:21:14 +0600

    code 1674A

---
+++ Practice/codeforces/1674B.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define sf(n) scanf("%lld",&n);
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define nl cout<<"\n";
+#define PB push_back
+#define VST(V) sort(V.begin(),V.end())
+#define VSTrev(V) sort(V.begin(),V.end(),greater<long long int>())
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define  base1 129
+#define  base2 137
+#define  MOD1  1479386893
+#define  MOD2  1928476349
+#define MAX 2000010
+#define T(n) printf("test : %d\n",n);
+ll dx[]= {1,-1,0,0,1,-1,-1,1};
+ll dy[]= {0,0,1,-1,1,1,-1,-1};
+ll knx[]= {2,2,1,-1,-2,-2,1,-1};
+ll kny[]= {1,-1,2,2,1,-1,-2,-2};
+int main()
+{
+    map<string,ll>mp;
+    ll cnt=1;
+    for(ll i=0; i<26; i++)
+    {
+        for(ll j=0; j<26; j++)
+        {
+            if(i!=j)
+            {
+                string s;
+                s+=(i+'a');
+                s+=(j+'a');
+                mp[s]=cnt++;
+            }
+        }
+    }
+    //freopen("1input.txt","r",stdin);
+    //freopen("1output.txt","w",stdout);
+    fast;
+    ll tcase=1;
+    cin>>tcase;
+    for(ll test=1; test<=tcase; test++)
+    {
+        string s;
+        cin>>s;
+        cout<<mp[s]<<"\n";
+    }
+    return 0;
+}
+
+
+
+
+
