515abe6c121a589b02ed7925a6e963e03087d6ea
unknown <arman.cse4.bu@gmail.com>
2022-05-05 15:59:01 +0600

    code 1674C

---
+++ Practice/codeforces/1674C.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define sf(n) scanf("%lld",&n);
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define nl cout<<"\n";
+#define PB push_back
+#define VST(V) sort(V.begin(),V.end())
+#define VSTrev(V) sort(V.begin(),V.end(),greater<long long int>())
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define  base1 129
+#define  base2 137
+#define  MOD1  1479386893
+#define  MOD2  1928476349
+#define MAX 2000010
+#define T(n) printf("test : %d\n",n);
+ll dx[]= {1,-1,0,0,1,-1,-1,1};
+ll dy[]= {0,0,1,-1,1,1,-1,-1};
+ll knx[]= {2,2,1,-1,-2,-2,1,-1};
+ll kny[]= {1,-1,2,2,1,-1,-2,-2};
+int main()
+{
+    //freopen("1input.txt","r",stdin);
+    //freopen("1output.txt","w",stdout);
+    fast;
+    ll tcase=1;
+    cin>>tcase;
+    for(ll test=1; test<=tcase; test++)
+    {
+        ll n,sum=0;
+        string s,t;
+        cin>>s>>t;
+        ll siz=s.size();
+        ll cka=0,ckother=0;
+        for(ll i=0; t[i]!='\0'; i++)
+        {
+            if(t[i]=='a')
+            {
+                cka=1;
+            }
+            else
+            {
+                ckother=1;
+            }
+        }
+        if(ckother)
+        {
+            if(cka)
+            {
+                cout<<"-1\n";
+            }
+            else
+            {
+                ll ans=pow(2,siz);
+                cout<<ans<<"\n";
+            }
+        }
+        else
+        {
+            ll ans=1;
+            if((ll)t.size()>1)
+            {
+                ans=-1;
+            }
+            cout<<ans<<"\n";
+        }
+    }
+    return 0;
+}
+
+
+
+
