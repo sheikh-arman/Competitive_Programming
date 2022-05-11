c8fade3a1e0253a312df0b2b851689c86ad04fae
unknown <arman.cse4.bu@gmail.com>
2022-05-12 03:44:17 +0600

    code policy based

---
+++ Datastructure_Algorithms_NumberTheory/Policy_Based_DataStructure/Codeforces 1676H2.cpp
@@ -0,0 +1,66 @@
+/*
+    Sk arman Hossain
+    University of Barisal
+
+    Problem : Codeforces 1676H2
+    Solution : Policy Based Data Structure
+    Date: 12-5-22
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update> policy_set;
+
+#define sf(n) scanf("%lld",&n);
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define nl cout<<"\n";
+#define PB push_back
+#define VST(V) sort(V.begin(),V.end())
+#define VSTrev(V) sort(V.begin(),V.end(),greater<long long int>())
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define  base1 129
+#define  base2 137
+#define  MOD1  1479386893
+#define  MOD2  1928476349
+#define MAX 2000010
+#define T(n) printf("test : %d\n",n);
+ll dx[]= {1,-1,0,0,1,-1,-1,1};
+ll dy[]= {0,0,1,-1,1,1,-1,-1};
+ll knx[]= {2,2,1,-1,-2,-2,1,-1};
+ll kny[]= {1,-1,2,2,1,-1,-2,-2};
+int main()
+{
+    //freopen("1input.txt","r",stdin);
+    //freopen("1output.txt","w",stdout);
+    fast;
+    ll tcase=1;
+    cin>>tcase;
+    for(ll test=1; test<=tcase; test++)
+    {
+        ll n,sum=0;
+        cin>>n;
+        policy_set V;
+        for(ll i=0;i<n;i++){
+            ll a;
+            cin>>a;
+            ll pos=V.order_of_key(a);
+            sum+=i-pos;
+            V.insert(a);
+            //cout<<i+1<<" "<<i-pos<<" "<<sum<<" x\n";
+            //VST(V);
+        }
+        cout<<sum<<"\n";
+    }
+    return 0;
+}
+
+
+
+
+
