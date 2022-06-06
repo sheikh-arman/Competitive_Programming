b222e3ff622361f9bc996898a2eb560e1f223e38
unknown <arman.cse4.bu@gmail.com>
2022-06-07 01:22:06 +0600

    leet

--- Practice/leetcode/test.cpp
+++ Practice/leetcode/test.cpp
@@ -1,75 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
 class Solution
 {
 public:
-    bool mul(unsigned mid,unsigned divisor,unsigned dividend)
+    TreeNode* ans;
+    void sol(TreeNode* root, TreeNode* target)
     {
-        unsigned num=mid;
-        if(mid<divisor)
+        if(root->val==target->val)
         {
-            swap(mid,divisor);
+            ans=root;
+            return;
         }
-        unsigned res=0,ck=0;
-        while(divisor>0)
+        if(root->left!=NULL)
         {
-            if(divisor&1)
-            {
-                res+=mid;
-                divisor--;
-                ck=1;
-            }
-            if(divisor)
-                mid=mid<<1;
-            if(mid<0||res>dividend)return 0;
-            divisor=divisor>>1;
+            sol(root->left);
         }
-        return res<=dividend;
+        if(root->right!=NULL)
+        {
+            sol(root->right);
+        }
     }
-    int divide(int dividend, int divisor)
+
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target)
     {
-        if(dividend==0)
-        {
-            return 0;
-        }
-        if(divisor==-1)
-        {
-            if(dividend<=-2147483647)
-            {
-                return 2147483647;
-            }
-        }
-        if(divisor==1)
-        {
-            return dividend;
-        }
-        int sign=0;
-        if((dividend<0||divisor<0)&&!(dividend<0&&divisor<0))
-        {
-            sign=1;
-        }
-        unsigned  new_dividend=abs(dividend);
-        unsigned new_divisor=abs(divisor);
-        unsigned left=0,right=dividend;
-        if(new_dividend<new_divisor)
-        {
-            return 0;
-        }
-        while(left<=right)
-        {
-            unsigned mid=left+((right-left)>>1);
-            if(mul(mid,new_divisor,new_dividend))
-            {
-                left=mid+1;
-            }
-            else
-            {
-                right=mid-1;
-            }
-        }
-        int ans=right;
-        if(sign&&right>0)
-        {
-            ans=~ans+1;
-        }
-        return ans;
+        return sol(orginal,target);
     }
 };
