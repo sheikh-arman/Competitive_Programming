11b2c462e8f57d94d38f53bf9fb0d3ba17cdfe99
sheikh-arman <arman.cse4.bu@gmail.com>
2022-07-31 21:50:01 +0600

    test

---
+++ Practice/atcoder/abc262C.cpp
@@ -0,0 +1,102 @@
+/*
+    Sk arman Hossain
+    University of Barisal
+
+    Problem :
+    Solution :
+    Date:
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+
+#define sf(n) scanf("%lld",&n);
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define nl cout<<"\n";
+#define PB push_back
+#define VST(V) sort(V.begin(),V.end())
+#define VSTrev(V) sort(V.begin(),V.end(),greater<long long int>())
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define  base1 129
+#define  base2 137
+#define  MOD1  1479386893
+#define  MOD2  1928476349
+#define MAX 2000010
+#define T(n) printf("test : %d\n",n);
+ll dx[]= {1,-1,0,0,1,-1,-1,1};
+ll dy[]= {0,0,1,-1,1,1,-1,-1};
+ll knx[]= {2,2,1,-1,-2,-2,1,-1};
+ll kny[]= {1,-1,2,2,1,-1,-2,-2};
+int main()
+{
+    //freopen("1input.txt","r",stdin);
+    //freopen("1output.txt","w",stdout);
+    fast;
+    ll tcase=1;
+    cin>>tcase;
+    for(ll test=1; test<=tcase; test++)
+    {
+        ll n,m;
+        cin>>n>>m;
+        vector<ll>V;
+        for(ll i=0; i<m; i++)
+        {
+            ll a;
+            cin>>a;
+            V.PB(a);
+        }
+        VST(V);
+        vector<ll>dif;
+        for(ll i=0; i<m-1; i++)
+        {
+            dif.PB(abs(V[i]-V[(i+1)%m])-1);
+        }
+        ll x=V[0]-1;
+        x+=n-V[m-1];
+        dif.PB(x);
+        VST(dif);
+        reverse(dif.begin(),dif.end());
+        ll ans=0;
+        ll sum=0;
+        ll siz=dif.size();
+        for(ll i=0; i<siz; i++)
+        {
+            //cout<<dif[i]<<" ";
+            dif[i]-=sum;
+            //cout<<dif[i]<<"  x\n";
+            if(dif[i]>0)
+            {
+                if(dif[i]==1)
+                {
+                    ans+=1;
+                }
+                else
+                {
+                    ans+=dif[i]-1;
+                }
+            }
+            sum+=4;
+            //cout<<ans<<"\n";
+        }
+        cout<<n-ans<<"\n";
+//        for(ll i:dif)
+//        {
+//            cout<<i<<" \n";
+//        }
+
+
+    }
+    return 0;
+}
+
+
+
+
+
