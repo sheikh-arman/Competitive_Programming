5bda875b0079512c0d1b46991debb1586128bdb9
unknown <arman.cse4.bu@gmail.com>
2022-08-06 18:38:56 +0600

    m

--- Practice/codeforces/test.cpp
+++ Practice/codeforces/test.cpp
@@ -14,28 +14,13 @@ int main(){
         }
         else if(num%3==1){
             int c1,c2;
-            c1=c+(num/3)+2;
-            if(num%2==0){
-            c2 = c+(num/2);
-            }
-            if(c2<c1){
-                c=c2;
-            }
-            else{
-                c=c1;
-            }
+            c=c+(num/3)+1;
         }
         else if(num%3==2){
             c=c+(num/3)+1;
         }
-        else if(num%2==0){
-            c = c+(num/2);
-
-        }
         printf("%d\n",c);
-
     }
-
     return 0;
 }
 
