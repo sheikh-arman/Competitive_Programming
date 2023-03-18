abe26d600dc7c806e1ccd63b6c70fe64e4cb733a
sheikh-arman <arman.cse4.bu@gmail.com>
2023-03-18 23:57:38 +0600

    ad

--- Practice/A.cpp
+++ Practice/A.cpp
@@ -1,47 +1,60 @@
+/*
+    Sk arman Hossain
+    University of Barisal
+
+    Problem :
+    Solution :
+    Date:
+ */
+
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long 
-const int nn=2e5+7;
-ll a[nn];
-vector<int>v;
-int main(){
-	ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    cin>>t;
-    while(t--){    	
-	    ll n,k,d,w,i,j=1,s=1,s2=0,z=0,s3=0;
-	    cin>>n>>k>>d>>w;
-	    for(i=0;i<n;i++)cin>>a[i];
-	    sort(a,a+n);
-	    ll x=min(a[0]+w,a[n-1]),y=0;
-	    for(i=0;i<n-1;i++){
-	    	if(a[i]<=x) {y++;a[i]=x;}
-	    	else{
-	    		if(a[i]-x>d || (y+1)%k==1){
-		    		x=min(a[i]+w,a[n-1]);
-		    		a[i]=x;
-		    		y=1;
-	    		}
-	    		else y++;
-	    	}
-	    }
-	    s2=d+a[0];s3=0;s=1;
-	    for(i=0;i<n;i++){
-	    	s3++;
-	    	if(s3>k){
-	    		s++;
-	    		s2=a[i]+d;
-	    		s3=1;
-	    	}
-	    	else if(a[i]>s2){
-	    		s++;
-	    		s2=a[i]+d;	
-	    		s3=1;    		
-	    	}
-	    }
-	    cout<<s<<endl;
+typedef long long int ll;
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+
+#define sf(n) scanf("%lld",&n);
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define nl cout<<"\n";
+#define PB push_back
+#define VST(V) sort(V.begin(),V.end())
+#define VSTrev(V) sort(V.begin(),V.end(),greater<long long int>())
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define  base1 129
+#define  base2 137
+#define  MOD1  1479386893
+#define  MOD2  1928476349
+#define MAX 2000010
+#define T(n) printf("test : %d\n",n);
+ll dx[]= {1,-1,0,0,1,-1,-1,1};
+ll dy[]= {0,0,1,-1,1,1,-1,-1};
+ll knx[]= {2,2,1,-1,-2,-2,1,-1};
+ll kny[]= {1,-1,2,2,1,-1,-2,-2};
+int main()
+{
+    //freopen("1input.txt","r",stdin);
+    //freopen("1output.txt","w",stdout);
+    fast;
+    ll tcase=1;
+    cin>>tcase;
+    for(ll test=1; test<=tcase; test++)
+    {
+        ll n;
+        cin>>n;
+        
+
+
+
     }
-	return 0;
+    return 0;
+ ///*****************************  ALHAMDULILLAH  *****************************/
 }
-			
+
+
+
+
+
